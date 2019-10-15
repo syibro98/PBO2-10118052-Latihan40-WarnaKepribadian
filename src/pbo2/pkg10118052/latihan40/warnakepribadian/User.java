db290fa0e4ec9357fd058f01bb3fb3f1559954cd
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-10-15 12:41:57 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan40/warnakepribadian/User.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author
+ * NAMA     : Bagus Syibro Malisi
+ * KELAS    : IF-2
+ * NIM      : 10118052
+ * Deskripsi Program : Program menentukan kepribadian seseorang bersarkan warna
+ * favorit. Disini kita belajar cara pewarnaan pada java app console serta
+ * program berbasis object
+ */
+public class User {
+    private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+}
